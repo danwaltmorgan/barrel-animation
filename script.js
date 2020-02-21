f73973c7e94228fa207234884e43931a9fdf7381
Dan Morgan <ltdan681@gmail.com>
2020-02-21 17:45:30 -0500

    moving arms and trash works

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const lid = document.querySelector('.top');
 const arm = document.querySelector('#right-arm');
+const forearm = document.querySelector("#lower-right-arm");
+const trash = document.querySelector("#trash");
 
 function swing() {
   if (!lid.classList.contains('open')) {
+    trash.style.opacity = "1";
     lid.classList.add('open')
     setTimeout(move => {
-      arm.style.transform = "rotate(-120deg)"
+      arm.style.transform = "rotate(-150deg)"
+      forearm.style.transform = "rotate(-90deg)"
     }, 700)
+
+    setTimeout(fall => {
+      trash.style.right = "-7vw";
+      setTimeout(disapear => {
+        trash.style.opacity = "0"
+      }, 500)
+    }, 1000)
   } else {
     lid.classList.remove('open')
     arm.style.transform = "rotate(-45deg)"
-
+    forearm.style.transform = "rotate(45deg)"
+    trash.style.right = "2vw"
   }
 }
