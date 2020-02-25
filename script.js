955b06a017e2de421d495cee0c960cc04983c732
Dan Morgan <ltdan681@gmail.com>
2020-02-25 16:15:02 -0500

    made barrel arms bigger and made him move to the right

--- script.js
+++ script.js
@@ -5,20 +5,30 @@ const trash = document.querySelector("#trash");
 const barrel = document.querySelector(".barrel")
 
 function swing() {
+  barrel.style.left = "0vw"
+  setTimeout(tip => {
+    barrel.style.transform = "rotate(0deg)"
+  }, 100)
+  // setTimeout(tip => {
+    // barrel.style.transform = "rotate(0deg)"
+  // }, 500)
   if (!lid.classList.contains('open')) {
-    trash.style.opacity = "1";
-    lid.classList.add('open')
+    setTimeout(open => {
+      trash.style.opacity = "1";
+      lid.classList.add('open')
+    }, 1000)
     setTimeout(move => {
       arm.style.transform = "rotate(-150deg)"
       forearm.style.transform = "rotate(-90deg)"
-    }, 700)
+    }, 1700)
 
     setTimeout(fall => {
-      trash.style.right = "-7vw";
+      trash.style.right = "-8vw";
       setTimeout(disapear => {
         trash.style.opacity = "0"
-      }, 500)
-    }, 1000)
+      }, 1000)
+    }, 2000)
+
   } else {
     lid.classList.remove('open')
     arm.style.transform = "rotate(-45deg)"
